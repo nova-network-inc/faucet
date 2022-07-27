46de5bba82288410fe523c2b8b0f9e7727096fa9
SuperDev321 <superman.dev2022@mail.ru>
2022-07-27 04:46:53 +0200

    Update NBX faucet

--- src/webapp/index.js
+++ src/webapp/index.js
@@ -1,6 +1,6 @@
-const h = require('h')
-const fetch = require('cross-fetch');
-const EthQuery = require('eth-query')
+const h = require("h");
+const fetch = require("cross-fetch");
+const EthQuery = require("eth-query");
 
 var state = {
   isLoading: true,
@@ -13,64 +13,71 @@ var state = {
   fromBalance: null,
   errorMessage: null,
 
-  transactions: []
-}
+  transactions: [],
+};
 
-window.addEventListener('load', startApp)
+window.addEventListener("load", startApp);
 
-function startApp () {
-
+function startApp() {
   // attempt to get provider from environment
-  let provider
+  let provider;
   if (global.ethereum) {
-    provider = global.ethereum
+    provider = global.ethereum;
   } else if (global.web3) {
-    provider = global.web3.currentProvider
+    provider = global.web3.currentProvider;
   }
 
   // display warning if incompatible
   if (!provider) {
     // abort
-    render(h('span', 'No ethereum provider detected. Install a web-enabled wallet (eg MetaMask metamask.io) to continue'))
-    return
+    render(
+      h(
+        "span",
+        "No ethereum provider detected. Install a web-enabled wallet (eg MetaMask metamask.io) to continue"
+      )
+    );
+    return;
   }
 
   // create query helper
-  global.ethQuery = new EthQuery(provider)
-  global.provider = provider
+  global.ethQuery = new EthQuery(provider);
+  global.provider = provider;
 
-  renderApp()
-  updateStateFromNetwork()
-  setInterval(updateStateFromNetwork, 4000)
+  renderApp();
+  updateStateFromNetwork();
+  setInterval(updateStateFromNetwork, 4000);
 }
 
-function updateStateFromNetwork () {
-  getNetwork()
-  getAccounts()
-  getBalances()
-  renderApp()
+function updateStateFromNetwork() {
+  getNetwork();
+  getAccounts();
+  getBalances();
+  renderApp();
 }
 
-function getNetwork () {
-  global.provider.sendAsync({ id: 1, jsonrpc: '2.0', method: 'net_version' }, function (err, res) {
-    if (err) return console.error(err)
-    if (res.error) return console.res.error(res.error)
-    var network = res.result
-    state.network = network
-    renderApp()
-  })
+function getNetwork() {
+  global.provider.sendAsync(
+    { id: 1, jsonrpc: "2.0", method: "net_version" },
+    function (err, res) {
+      if (err) return console.error(err);
+      if (res.error) return console.res.error(res.error);
+      var network = res.result;
+      state.network = network;
+      renderApp();
+    }
+  );
 }
 
-function getAccounts () {
+function getAccounts() {
   global.ethQuery.accounts(function (err, accounts) {
-    if (err) return console.error(err)
-    var address = accounts[0]
-    if (state.userAddress === address) return
-    state.userAddress = address
-    state.fromBalance = null
-    getBalances()
-    renderApp()
-  })
+    if (err) return console.error(err);
+    var address = accounts[0];
+    if (state.userAddress === address) return;
+    state.userAddress = address;
+    state.fromBalance = null;
+    getBalances();
+    renderApp();
+  });
 }
 
 /*
@@ -78,241 +85,241 @@ function getAccounts () {
  * Read more here:
  * https://medium.com/metamask/eip-1102-preparing-your-dapp-5027b2c9ed76
  */
-async function requestAccounts () {
-  const provider = global.provider
-  if ('enable' in provider) {
+async function requestAccounts() {
+  const provider = global.provider;
+  if ("enable" in provider) {
     try {
-      const accounts = await provider.enable()
-      getAccounts()
-      return accounts[0]
+      const accounts = await provider.enable();
+      getAccounts();
+      return accounts[0];
     } catch (err) {
-      window.alert('Your web3 account is currently locked. Please unlock it to continue.')
+      window.alert(
+        "Your web3 account is currently locked. Please unlock it to continue."
+      );
     }
   } else {
     // Fallback to old way if no privacy mode available
     if (state.userAddress) {
-      return state.userAddress
+      return state.userAddress;
     } else {
-      window.alert('Your web3 account is currently locked. Please unlock it to continue.')
-      throw new Error('web3 account locked')
+      window.alert(
+        "Your web3 account is currently locked. Please unlock it to continue."
+      );
+      throw new Error("web3 account locked");
     }
   }
 }
 
-function getBalances () {
+function getBalances() {
   if (state.faucetAddress) {
     global.ethQuery.getBalance(state.faucetAddress, function (err, result) {
-      if (err) return console.error(err)
-      state.faucetBalance = (parseInt(result, 16) / 1e18).toFixed(2)
-      renderApp()
-    })
+      if (err) return console.error(err);
+      state.faucetBalance = (parseInt(result, 16) / 1e18).toFixed(2);
+      renderApp();
+    });
   }
 
   if (state.userAddress) {
     global.ethQuery.getBalance(state.userAddress, function (err, result) {
-      if (err) return console.error(err)
-      state.fromBalance = (parseInt(result, 16) / 1e18).toFixed(2)
-      renderApp()
-    })
+      if (err) return console.error(err);
+      state.fromBalance = (parseInt(result, 16) / 1e18).toFixed(2);
+      renderApp();
+    });
   }
 }
 
-function renderApp () {
+function renderApp() {
   // if (state.isLoading) {
   //   return render(h('span', 'web3 detected - Loading...'))
   // }
 
   // render wrong network warning
-  if (state.network === '1') {
+  if (state.network !== "107") {
     return render([
-
-      h('section.container', [
-        h('div.panel.panel-default', [
-          h('div.panel-heading', [
-            h('h3', 'network')
+      h("section.container", [
+        h("div.panel.panel-default", [
+          h("div.panel-heading", [h("h3", "Network Error")]),
+          h("div.panel-body", [
+            "You are currently connected to the wrong network. Please switch over to Nebula Testnet",
           ]),
-          h('div.panel-body', [
-            'currently on mainnet - please select the correct test network'
-          ])
-        ])
-      ])
-
-    ])
+        ]),
+      ]),
+    ]);
   }
 
   // render faucet ui
   render([
-
-    h('nav.navbar.navbar-default', [
-      h('h1.container-fluid', 'MetaMask Ether Faucet')
+    h("nav.navbar.navbar-default.text-center", [
+      h("h1.container-fluid", "Nebula Testnet Faucet"),
     ]),
 
-    h('section.container', [
-
-      h('div.panel.panel-default', [
-        h('div.panel-heading', [
-          h('h3', 'faucet')
-        ]),
-        h('div.panel-body', [
-          h('div', 'address: ' + state.faucetAddress),
-          h('div', 'balance: ' + formatBalance(state.faucetBalance)),
-          h('button.btn.btn-success', 'request 1 ether from faucet', {
+    h("section.container", [
+      h("div.panel.panel-default", [
+        h("div.panel-heading", [h("h3", "Faucet")]),
+        h("div.panel-body", [
+          h("div", "Faucet Wallet: " + state.faucetAddress),
+          h("div", "Available Balance: " + formatBalance(state.faucetBalance)),
+          h("button.btn.btn-success", "Request 0.1 NBX", {
             style: {
-              margin: '4px'
+              margin: "4px",
             },
             // disabled: state.userAddress ? null : true,
-            click: getEther
-          })
-        ])
+            click: getEther,
+          }),
+        ]),
       ]),
 
-      h('div.panel.panel-default', [
-        h('div.panel-heading', [
-          h('h3', 'user')
+      h("div.panel.panel-default", [
+        h("div.panel-heading", [h("h3", "Your Wallet")]),
+        h("div.panel-body", [
+          h("div", "Your Address: " + state.userAddress),
+          h("div", "Your Balance: " + formatBalance(state.fromBalance)),
+          h("div", "Make a Donation:"),
+          h("button.btn.btn-warning", "1 NBX", {
+            style: {
+              margin: "4px",
+            },
+            // disabled: state.userAddress ? null : true,
+            click: sendTx.bind(null, 1),
+          }),
+          h("button.btn.btn-warning", "10 NBX", {
+            style: {
+              margin: "4px",
+            },
+            // disabled: state.userAddress ? null : true,
+            click: sendTx.bind(null, 10),
+          }),
+          h("button.btn.btn-warning", "100 NBX", {
+            style: {
+              margin: "4px",
+            },
+            // disabled: state.userAddress ? null : true,
+            click: sendTx.bind(null, 100),
+          }),
         ]),
-        h('div.panel-body', [
-          h('div', 'address: ' + state.userAddress),
-          h('div', 'balance: ' + formatBalance(state.fromBalance)),
-          h('div', 'donate to faucet:'),
-          h('button.btn.btn-warning', '1 ether', {
-            style: {
-              margin: '4px'
-            },
-            // disabled: state.userAddress ? null : true,
-            click: sendTx.bind(null, 1)
-          }),
-          h('button.btn.btn-warning', '10 ether', {
-            style: {
-              margin: '4px'
-            },
-            // disabled: state.userAddress ? null : true,
-            click: sendTx.bind(null, 10)
-          }),
-          h('button.btn.btn-warning', '100 ether', {
-            style: {
-              margin: '4px'
-            },
-            // disabled: state.userAddress ? null : true,
-            click: sendTx.bind(null, 100)
-          })
-        ])
       ]),
 
-      h('div.panel.panel-default', [
-        h('div.panel-heading', [
-          h('h3', 'transactions')
-        ]),
-        h('div.panel-body', {
-          style: {
-            'flex-direction': 'column',
-            display: 'flex'
-          }
-        }, (
+      h("div.panel.panel-default", [
+        h("div.panel-heading", [h("h3", "Transactions")]),
+        h(
+          "div.panel-body",
+          {
+            style: {
+              "flex-direction": "column",
+              display: "flex",
+            },
+          },
           state.transactions.map((txHash) => {
-            return link(`https://ropsten.etherscan.io/tx/${txHash}`, txHash)
+            return link(
+              `https://testnet.novascan.io/transactions/${txHash}`,
+              txHash
+            );
           })
-        ))
-      ])
-
+        ),
+      ]),
     ]),
 
-    state.errorMessage ? h('div', { style: { color: 'red' } }, state.errorMessage) : null
-
-  ])
+    state.errorMessage
+      ? h("div.text-center", { style: { color: "red" } }, state.errorMessage)
+      : null,
+  ]);
 }
 
-function link (url, content) {
-  return h('a', { href: url, target: '_blank' }, content)
+function link(url, content) {
+  return h("a", { href: url, target: "_blank" }, content);
 }
 
-async function getEther () {
-  const account = await requestAccounts()
+async function getEther() {
+  const account = await requestAccounts();
 
   // We already prompted to unlock in requestAccounts()
-  if (!account) return
+  if (!account) return;
 
-  var uri = `${window.location.href}v0/request`
-  var data = account
+  var uri = `${window.location.href}v0/request`;
+  var data = account;
 
-  let res, body, err
-  
+  let res, body, err;
+
   try {
     res = await fetch(uri, {
-      method: 'POST',
+      method: "POST",
       body: data,
       headers: {
-        'Content-Type': 'application/rawdata'
-      }
-    })
-    body = await res.text()
+        "Content-Type": "application/rawdata",
+      },
+    });
+    body = await res.json();
   } catch (error) {
-    err = error
+    err = error;
   }
 
   // display error
   if (err) {
-    state.errorMessage = err || err.stack
-    return
+    state.errorMessage = err || err.stack;
+    return;
   }
 
   if (res.status === 420) {
-    state.errorMessage = `Being ratelimited... try again later`
-    return
+    state.errorMessage = `Being ratelimited... try again later`;
+    return;
   }
 
   if (!res.ok) {
-    state.errorMessage = `Error: ${res.status} ${res.statusText} ${body}`
-    return
+    state.errorMessage = body.error || body;
+    return;
   }
 
   // display error-in-body
   try {
-    if (body.slice(0, 2) === '0x') {
-      state.transactions.push(body)
-      state.errorMessage = null
+    if (body.slice(0, 2) === "0x") {
+      state.transactions.push(body);
+      state.errorMessage = null;
     } else {
-      state.errorMessage = body
+      state.errorMessage = body.error || body;
     }
   } catch (err) {
-    state.errorMessage = err || err.stack
+    state.errorMessage = err || err.stack;
   }
 
   // display tx hash
-  console.log('faucet response:', body)
-  updateStateFromNetwork()
+  console.log("faucet response:", body);
+  updateStateFromNetwork();
 }
 
-async function sendTx (value) {
-  const address = await requestAccounts()
-  if (!address) return
+async function sendTx(value) {
+  const address = await requestAccounts();
+  if (!address) return;
 
-  global.ethQuery.sendTransaction({
-    from: address,
-    to: state.faucetAddress,
-    value: '0x' + (value * 1e18).toString(16)
-  }, function (err, txHash) {
-    if (err) {
-      state.errorMessage = (err && err.stack)
-    } else {
-      console.log('user sent tx:', txHash)
-      state.errorMessage = null
-      state.transactions.push(txHash)
+  global.ethQuery.sendTransaction(
+    {
+      from: address,
+      to: state.faucetAddress,
+      value: "0x" + (value * 1e18).toString(16),
+    },
+    function (err, txHash) {
+      if (err) {
+        state.errorMessage = err && err.stack;
+      } else {
+        console.log("user sent tx:", txHash);
+        state.errorMessage = null;
+        state.transactions.push(txHash);
+      }
+      updateStateFromNetwork();
     }
-    updateStateFromNetwork()
-  })
+  );
 }
 
-function render (elements) {
-  if (!Array.isArray(elements)) elements = [elements]
-  elements = elements.filter(Boolean)
+function render(elements) {
+  if (!Array.isArray(elements)) elements = [elements];
+  elements = elements.filter(Boolean);
   // clear
-  document.body.innerHTML = ''
+  document.body.innerHTML = "";
   // insert
   elements.forEach(function (element) {
-    document.body.appendChild(element)
-  })
+    document.body.appendChild(element);
+  });
 }
 
-function formatBalance (balance) {
-  return balance ? balance + ' ether' : '...'
+function formatBalance(balance) {
+  return balance ? balance + " NBX" : "...";
 }
